1299a9865a7cfdc827b0d9014f41f7615ec8e4ff
qiafengchen <67124360+qiafengchen@users.noreply.github.com>
2022-07-31 21:05:44 -0400

    add carousel UI feature

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Carousel from '../Carousel/Carousel';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-
+      <Carousel />
       <main>{children}</main>
     </div>
   );
